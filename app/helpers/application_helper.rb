7c29678121cebe66e6c3c433892d6c1db0a98240
ayabeck <aki@AkNMB.local>
2015-09-16 18:55:28 +0900

    Fix capitalized head title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -40,7 +40,7 @@ module ApplicationHelper
     if page_title.empty?
       base_title
     else
-      "#{ base_title } | #{ page_title }"
+      "#{ base_title } #{ page_title.capitalize }"
     end
   end
 end
